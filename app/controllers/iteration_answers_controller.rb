617d781de7ba504e7ddfed5ff7a77095dbaad1b9
Philipp Schaefer <pschaefer@posteo.net>
2021-04-15 20:16:06 +0200

    create controllers

---
+++ app/controllers/iteration_answers_controller.rb
@@ -0,0 +1,2 @@
+class IterationAnswersController < ApplicationController
+end
---
+++ test/controllers/iteration_answers_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class IterationAnswersControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
