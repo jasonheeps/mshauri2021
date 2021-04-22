5ab8db32251938e3d71fbf7fa1510a053bc080bc
Kimon Haars <kimon.haars@gmail.com>
2021-04-22 20:16:08 +0200

    add base controller methods to application controller

--- app/controllers/iteration_answers_controller.rb
+++ app/controllers/iteration_answers_controller.rb
@@ -1,2 +1,6 @@
 class IterationAnswersController < ApplicationController
+  # I'm unsure if we need this
+  # In any case it needs to be defined in the Base-Controller
+  # before_action :require_current_user
+
 end
