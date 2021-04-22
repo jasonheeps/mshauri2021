6856ae30e27a4d86d405c237c49f8908eb42568d
Philipp Schaefer <pschaefer@posteo.net>
2021-04-22 19:05:53 +0200

    for home#welcome: add route user#create; copy view home#welcome; add css

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
+  # page rendered as root - when no user slug is given
   def welcome
+    @user = User.new
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  root to: 'home#welcome'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'home#welcome'
+
+  resources :users, only: [:create]
 end
