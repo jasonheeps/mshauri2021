8c856c262a3b930f07ca43ffa772a48ef672820d
Megan Grosz <megrosz@gmail.com>
2021-05-06 23:30:23 +0200

    recommendations routes

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -1,2 +1,27 @@
 class RecommendationsController < ApplicationController
+  before_action :require_current_user, except: %i[all show]
+  # before_action :require_current_iteration, except: %i[all show]
+  skip_after_action :verify_authorized, only: %i[all show]
+
+  def index
+    @recommendations = policy_scope(Recommendation)
+
+    @recommendations = @recommendations.joins(:iterations).where(iterations: { id: params[:iteration_id] }) if params[:iteration_id]
+
+    @recommendations = @recommendations.order(priority: :desc).distinct.to_a
+    return unless @recommendations.count < 7
+
+    @recommendations += Recommendation.general.to_a
+    @recommendations.uniq!
+    @recommendations.sort! { |a, b| b.priority <=> a.priority } # sorty by highest priorty first
+  end
+
+  def all
+    @recommendations = Recommendation.order(priority: :desc)
+    render '/api/recommendations/index'
+  end
+
+  def show
+    @recommendation = Recommendation.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,16 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :questions, only: %i[index show]
   resources :iteration_answers, only: %i[index create]
+  resources :recommendations, only: %i[show] do
+    # collection do
+    #   get :all
+    # end
+  end
 
   get '/:user_slug/i/:iteration_id/questions/:question_id', to: 'questions#show', as: :iteration_question
+  get '/:user_slug/i/:iteration_id/recommendations', to: 'recommendations#index', as: :recommendations
+  # get '/:user_slug/i/:iteration_id/recommendations/:recommendation_id', to: 'recommendations#show', as: :recommendations
+  # recommendations route from original app
+  # get '/:user_slug/i/:iteration_id/recommendations', to: 'home#index', as: :recommendations
 
 end
