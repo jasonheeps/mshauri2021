6a48ad0e8b892b802d520be66354b5c0509b261b
Philipp Schaefer <pschaefer@posteo.net>
2021-05-27 18:27:23 +0200

    add Iteration#starting_question_id to redirect new users to first question

--- app/models/iteration.rb
+++ app/models/iteration.rb
@@ -4,4 +4,13 @@ class Iteration < ApplicationRecord
   has_many :answers, through: :iteration_answers
   has_many :questions, through: :iteration_answers
   has_many :recommendations, through: :answers
+
+  # This method will return the first question in case the
+  # the iteration has no answer yet and then return the last answer's next question in
+  # case the iteration has answers
+  def starting_question_id
+    return iteration_answers.last.answer.next_question_id if iteration_answers.any?
+
+    Question.find_by(identifier: '1').id
+  end
 end
