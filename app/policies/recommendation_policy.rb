1cad249600deda7831a0207ec8fa4bd8c310f959
Morrism1 <morrismontana0@gmail.com>
2021-05-27 19:52:26 +0200

    Add other policies

---
+++ app/policies/recommendation_policy.rb
@@ -0,0 +1,13 @@
+class RecommendationPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      return scope.none unless user
+
+      if user.admin?
+        scope.all
+      else
+        scope.joins(:iterations).where(iterations: { user_id: user.id })
+      end
+    end
+  end
+end
